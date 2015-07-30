be46427925dee10ca23023749d6ef94d1698fcb2
Sam Buck <swb5075@uncw.edu>
2015-07-30 13:25:49 -0400

    Documentation Typos

--- DevelopingRelease/WinnowDevel/Tests/winnow_unittest.py
+++ DevelopingRelease/WinnowDevel/Tests/winnow_unittest.py
@@ -46,7 +46,7 @@ class WinnowTest(unittest.TestCase):
         self.win.load_kt()
         gen = self.win.do_analysis()
         a = gen.next()[1]
-        self.assertEqual(format_float(a),
+        self.assertEqual(format_float(a[1:]),
                          [0.05896121, 0.18211783, -0.03838186, 0.43427679, 0, 384,
                           2816, 35, 0.0, 0.12, 0.12952087, 0.87047913, 0.0, 0.88, 0.0, 1.0, -0.12])
         gen.close()
@@ -57,7 +57,8 @@ class WinnowTest(unittest.TestCase):
         score_column = (0.003, 0.65, 0.004, 0.006, 0.078, 0.003, 0.0001, 0.513, 0.421, 0.0081, 0.043, 0.98)
         self.win.beta_true_false = (1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1)
         beta_column = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-        a = format_float(self.win.do_gwas(score_column, beta_column)[1])
+        a = self.win.do_gwas("Name", score_column, beta_column)[1][1:]
+        a = format_float(a)
         self.assertEqual(a,
                          [47.08333333, 5.91666667, 0.16903085, 0.56944444, 4.0, 3.0, 3.0, 2.0, 0.66666667, 0.5,
                           0.41666667, 0.58333333, 0.66666667, 0.5, 0.57142857, 0.42857143, 0.16666667])
